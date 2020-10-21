a85bf7efc4d33a03153711b074cd3e103500597a
Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com>
2020-10-21 15:55:54 -0300

    loja semi pronta

--- controller/Loja.js
+++ controller/Loja.js
@@ -1,11 +1,34 @@
 const Pacotes =  require('../model/Pacotes');
 const Categorias = require('../model/Categorias');
+const { populate } = require('../model/Categorias');
 
 exports.formLoja = (req, res, next) => {
-    Pacotes.find().populate('categorias').exec(
-        (err, pacote) =>{
-            if(err) res.redirect(`/erro?erro=${erro}`);
-            res.render('loja/formLoja', {pacote});            
+    Pacotes.find({  })
+        .populate('categorias').exec(
+        (erro, pacote) =>{
+            if(erro) res.redirect(`/erro?erro=${erro}`);
+            res.render('loja/formLoja', {pacote});
         }
     )
+}
+
+exports.formPacote = (req, res, next) => {
+    Pacotes.find({ _id: req.params.id})
+        .populate('categorias').exec(
+            (erro, pacote) => {
+                if(erro) res.redirect(`/erro?erro=${erro}`);
+                res.render('loja/formPacote', {pacote});
+            }
+        )
+}
+
+exports.formCategoria = (req, res, next) => {
+    Pacotes.find({ categorias: req.params.id })
+        .exec(
+            (erro, pacote) => {
+                if(erro) res.redirect(`/erro?erro=${erro}`);
+                res.render('loja/formCategoria', {pacote});
+            }
+        )
+
 }
--- routes/routeLoja.js
+++ routes/routeLoja.js
@@ -3,5 +3,7 @@ const router = Router();
 const controller =  require('../controller/Loja');
 
 router.get('/', controller.formLoja);
+router.get('/pacote/:id', controller.formPacote);
+router.get('/categoria/:id', controller.formCategoria);
  
 module.exports = router;
